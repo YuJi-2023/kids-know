3857b89a1e05bee4dc60b5185739a8901ae0af77
Yu Ji <sophia.jiyu@gmai.com>
2023-09-13 12:57:23 +1000

    setup workflow

--- src/index.js
+++ src/index.js
@@ -1,17 +1,100 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react';
+import { Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { CardChecklist, Trash } from "react-bootstrap-icons";
+import Container from "react-bootstrap/Container";
+import FormControl from "react-bootstrap/FormControl";
+import InputGroup from "react-bootstrap/InputGroup";
+import Navbar from "react-bootstrap/Navbar";
+import { createRoot } from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function TodoList() {
+  return [
+    {
+      id: 1,
+      title: "Dress-up",
+      completed: false,
+    },
+    {
+      id: 2,
+      title: "Pack-up",
+      completed: false,
+    },
+    {
+      id: 3,
+      title: "Breakkie",
+      completed: false,
+    },
+    {
+      id: 4,
+      title: "School-time",
+      completed: false,
+    },
+    {
+      id: 5,
+      title: "Free-play",
+      completed: false,
+    },
+    {
+      id: 6,
+      title: "Homework",
+      completed: false,
+    },
+  ];
+}
+
+function TodoItem(props) {
+  return (
+     <InputGroup key={props.id}>
+      <InputGroup.Checkbox defaultChecked={props.completed} onChange={props.onToggle} />
+      <FormControl
+        defaultValue={props.title}
+        style={{
+          textDecoration: props.completed ? "line-through" : "none"
+        }}
+        />
+        <Button variant="outline-danger" onClick={props.onDelete}>
+          <Trash />
+        </Button>
+     </InputGroup>
+  );
+}
+
+function App() {
+  const [todos, setTodos] = useState(TodoList());
+
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <CardChecklist /> My Daily Schedule
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Container>
+        {todos.map((todo) => (  
+          <TodoItem
+            key={todo.id}
+            title={todo.title}
+            completed={todo.completed}
+            onDelete={() =>{ 
+              setTodos(todos.filter((x) => x.id !== todo.id));
+            }}
+            onToggle={() =>{
+              setTodos(
+                todos.map((x) =>
+                  x.id === todo.id ? { ...x, completed: !x.completed } : x
+                )
+              )
+            }}
+          />
+        ))}
+      </Container>
+    </> 
+  );
+}
+
+createRoot(document.getElementById('root')).render(<App />);
