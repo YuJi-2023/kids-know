49896e2ad8c769aaaa746f3ca4bc1f0e67573123
YuJi-2023 <sophia.jiyu@gmail.com>
2024-03-28 22:23:22 +1100

    update HomePageSliders

--- src/components/HomePage/AboutUs.js
+++ src/components/HomePage/AboutUs.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Image from "react-bootstrap/Image";
 import Carousel from "react-bootstrap/Carousel";
 import slide_1 from "../../assests/HomePageSlides/slide_1.jpg";
 import slide_2 from "../../assests/HomePageSlides/slide_2.jpg";
+import slide_3 from "../../assests/HomePageSlides/slide_3.jpg";
 import { Container } from "react-bootstrap";
 const AboutUs = () => {
   return (
@@ -10,16 +10,35 @@ const AboutUs = () => {
       <Container className="bg-primary px-5 d-block w-50" fluid>
         <Carousel className="py-5">
           <Carousel.Item>
-            <img className="d-block w-100 rounded" src={slide_1} alt="First slide" />
+            <img
+              className="d-block w-100 rounded"
+              src={slide_1}
+              alt="First slide"
+            />
             <Carousel.Caption>
-              <h3>First slide label</h3>
+              <h3>Self-guided Learning</h3>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-            <img className="d-block w-100 rounded" src={slide_2} alt="First slide" />
+            <img
+              className="d-block w-100 rounded"
+              src={slide_2}
+              alt="Second slide"
+            />
             <Carousel.Caption>
-              <h3>Second slide label</h3>
+              <h3>A Good Learner</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-100 rounded"
+              src={slide_3}
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3> Be Responsible</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Carousel.Caption>
           </Carousel.Item>
